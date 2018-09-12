a5b1d5b7401205462d2296981681385ec2f17a8e
Erickson Ferreira <erickson.rinho@hotmail.com>
2018-09-11 23:38:50 -0300

    Cud com login inicial

---
+++ app/Evento.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Evento extends Model {
+	protected $fillable = ['nome', 'email', 'site', 'descricao'];
+	protected $guarded = ['id', 'created_at', 'update_at'];
+	protected $table = 'evento2';
+
+}
