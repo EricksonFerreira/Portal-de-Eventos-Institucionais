ac9b2f2cde890330924f78494cce44fc0a730c8f
Tuanno <tuannodanyllo26@gmail.com>
2018-11-14 17:18:30 -0300

    Comecando a integracao

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,64 +17,67 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/semantic.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/icon.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                @endif
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+        <div class="ui large fixed stackable menu">
+            <a href="" class="item">
+                <img src="../../../public/img/icone/ifpe.png" alt="">
+            </a>
+            <a href="eventos.php" class="item">
+                <i class="calendar green icon"></i><span style="color: green">Ver Eventos</span>
+            </a>
+            <div class=" right menu">
+                <div class="item">
+                    <div class="ui action input"> 
+                        <input type="text" placeholder="Procurar evento">
+                        <button class="ui icon green button"><i class="search icon"></i></button>
+                    </div>
                 </div>
-            </div>
-        </nav>
+        @guest      
+                <a href="{{ route('login') }}"class="item">
+                    <button class="ui green inverted button">
+                        <i class="user icon"></i>{{ __('Login') }}
+                    </button>
+                </a>
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}"class="item">
+                    <button class="ui green inverted button">
+                        <i class="user icon"></i>{{ __('Registro') }}
+                    </button>
+                </a>
+                @endif
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+            </div>
+        </div>
+
+        @else
+        <div class="item">  
+            <button class="ui green inverted button">
+                <i class="user icon"></i>{{ Auth::user()->name }}
+            </button>
+        </div>                            
+
+        <a href="{{ route('logout') }}"class="item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <button class="ui green inverted button">
+                {{ __('Logout') }}
+                <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                    @csrf
+                </form> 
+
+            </button>
+        </a>
+
+
     </div>
+        @endguest
+</div>
+
+<main class="py-4">
+    @yield('content')
+</main>
 </body>
 </html>
