ad1d743a2b32fef9cdf31c0d0162c92dd9bad164
Erickson <erickson.rinho@hotmail.com>
2018-11-26 12:05:46 -0300

    Corrigindo erros nas Factorys

--- database/factories/AtividadeFactory.php
+++ database/factories/AtividadeFactory.php
@@ -8,13 +8,12 @@ use App\Palestrante;
 $factory->define(App\Atividade::class, function (Faker $faker) {
     $date = $faker->date('Y-m-d');
 	return [
-        'evento_id' => Evento::where(['campus' => 'igarassu'])->get()->random()->id,
+        'evento_id' => Evento::get()->random()->id,
 
         'palestrante_id' => Palestrante::get()->random()->id,
         'titulo' => $faker->word,
         'descricao' => $faker->paragraph,
         'confirmacao' => $faker->boolean,
-        'data' => $date,
         'hora_inicio' => '08:00',
         'hora_fim' => '16:00'
     ];
--- database/factories/PalestranteFactory.php
+++ database/factories/PalestranteFactory.php
@@ -7,7 +7,7 @@ use App\Evento;
 $factory->define(App\Palestrante::class, function (Faker $faker) {
     return [
     	'user_id' 	=> User::where(['role' => 'participante'])->get()->random()->id,
-    	'evento_id' => Evento::where(['campus' => 'igarassu'])->get()->random()->id,
+    	'evento_id' => Evento::get()->random()->id,
 			'descricao' => $faker->paragraph
     ];
 });
