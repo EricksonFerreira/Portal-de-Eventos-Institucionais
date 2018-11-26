c615056815ce4f53f314f4a3b57c21e7ff934816
Erickson <erickson.rinho@hotmail.com>
2018-11-26 12:06:51 -0300

    Corrigindo erros nas rotas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::resource('evento', 'EventoController');
 
 /*Resource que facilita chamar uma função do Controller*/
 Route::resource('atividade', 'AtividadeController');
+Route::get('/atividade/create/{id}', 'AtividadeController@create')->name('atividade.create');
 
 /* rota tradicional do método Auth*/
 Auth::routes();
