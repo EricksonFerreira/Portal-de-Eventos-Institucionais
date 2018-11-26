8bba01339c90e6acc8cfb00060379a21de9d77a4
Erickson <erickson.rinho@hotmail.com>
2018-11-26 12:26:31 -0300

    Em andamento - Criar uma unica view para criar e editar evento

--- resources/views/criar-evento.blade.php
+++ resources/views/criar-evento.blade.php
@@ -28,7 +28,11 @@
 		<div class="ui segment">
 			<div class="ui vertically divided grid">
 				<div class="column">
+				@isset($atividade)
+				<form action="{{route('evento.update', $eventos->id)}}" class="ui form" id="cadastro" method="post" enctype="multipart/form-data">{{ csrf_field() }}{{method_field('PUT')}}
+				@else
 					<form action="{{route('evento.store')}}" class="ui form" id="cadastro" method="post" enctype="multipart/form-data">
+				@endisset
 						<center>
 							<h2 class="ui  header">Cadastro do Evento</h2>
 						</center>
@@ -36,32 +40,32 @@
 						<strong><h3 class="ui dividing header">Sobre o evento</h3></strong>
 						<div class="field">
 							<br><label>Nome*
-								<input type="text" name="nome" placeholder="Nome do evento" value="{{old('nome')}}" >
+								<input type="text" name="nome" placeholder="Nome do evento" value="{{old('nome',$eventos->nome ?? '')}}"">
 							</label>
 							<div class="field">
 								<br><label>Descrição*
-									<textarea placeholder="Descrição do Evento" name="descricao">{{old('descricao')}}</textarea>
+									<textarea placeholder="Descrição do Evento" name="descricao">{{old('descricao',$eventos->descricao ?? '')}}</textarea>
 								</label>
 							</div>
 							<div class="three fields">
 								<div class="field">
 									<br><label>Email*
-										<input type="text" name="email" placeholder="Email para contato"  value="{{old('email')}}">
+										<input type="text" name="email" placeholder="Email para contato"  value="{{old('email',$eventos->email ?? '')}}">
 									</label>
 								</div>
 								<div class="field">
 									<br><label>Telefone*
-										<input type="text" name="telefone" placeholder="Telefone para contato" value="{{old('telefone')}}">
+										<input type="text" name="telefone" placeholder="Telefone para contato" value="{{old('telefone',$eventos->telefone ?? '')}}">
 									</label>
 								</div>								
 								<div class="field">
 									<br><label>Vagas*
-										<input type="number" name="vagas" value="{{old('vagas')}}">
+										<input type="number" name="vagas" value="{{old('vagas',$eventos->vagas ?? '')}}">
 									</label>
 								</div>
 								<div class="field">
 									<br><br><label for="file" class="ui icon green inverted button"><i class="file image icon"></i> Adicionar-Imagem
-										<input type="file" name="imagem" placeholder="Telefone para contato"   class="" value="{{old('imagem')}}" style="display: none;" id="file" >
+										<input type="file" name="imagem" placeholder="Telefone para contato"   class="" value="{{old('imagem',$eventos->imagem ?? '')}}" style="display: none;" id="file" >
 									</label>
 								</div>
 							</div>
