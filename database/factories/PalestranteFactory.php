7a3a19b243dd7f285908cd0ea7d6e09972bed8f3
jackson29 <jacksonssantos@bol.com.br>
2018-11-26 15:15:37 -0300

    adicionando o campo nome do PalestranteFactory

--- database/factories/PalestranteFactory.php
+++ database/factories/PalestranteFactory.php
@@ -8,6 +8,7 @@ $factory->define(App\Palestrante::class, function (Faker $faker) {
     return [
     	'user_id' 	=> User::where(['role' => 'participante'])->get()->random()->id,
     	'evento_id' => Evento::get()->random()->id,
+    	'nome' => $faker->name,
 			'descricao' => $faker->paragraph
     ];
 });
