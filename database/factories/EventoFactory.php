4355b11e6fbd18052e1b832cbfbe4ef161111d5e
Erickson <erickson.rinho@hotmail.com>
2018-11-27 12:08:31 -0300

    Correção no EventoFactory

--- database/factories/EventoFactory.php
+++ database/factories/EventoFactory.php
@@ -4,7 +4,8 @@ use Faker\Generator as Faker;
 use App\User;
 
 $factory->define(App\Evento::class, function (Faker $faker) {
-    $date = $faker->date('Y-m-d');
+    // $date = $faker->date('Y-m-d');
+    $date = $faker->dateTimeBetween('-1 month', '+2 month');
     return [
         'user_id' => User::where(['role' => 'manager'])->get()->random()->id,
         'nome' => $faker->word,
@@ -16,7 +17,8 @@ $factory->define(App\Evento::class, function (Faker $faker) {
         'vagas' => $faker->numberBetween(100, 500),
         'inicio_evento' => $date,
         'hora_inicio' => '08:00',
-        'fim_evento' => $date,
+        // 'fim_evento' => $date,
+        'fim_evento' => $faker->dateTimeBetween($date, '+2 month'),
         'hora_fim' => '16:00'
     ];
 });
