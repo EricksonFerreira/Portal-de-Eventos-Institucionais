a904560cf7d4e06b59805a6edcc9a9145843a0e9
Erickson <erickson.rinho@hotmail.com>
2018-11-27 12:07:47 -0300

    Colocando os cards no component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,7 @@
         @else
         <?php date_default_timezone_set("America/Recife");
 setlocale(LC_ALL, 'pt_BR');
-$created 	= date_create();?>
+?>
 		<!-- Se ela existir vai aparecer isso-->			
 		@can('criar-evento')
 			<div class="column">
@@ -51,49 +51,17 @@ $created 	= date_create();?>
 		@endcan
 		<!-- Listar todos os atributos dos itens já cadastrados na tabela-->
 		<center><div style="width: 100%;">			
-		 @foreach($eventos as  $evento)
-		<?php 
-			$date 		= date_format($created, 'd-m-Y');
-			$iniDt=date('d-m-Y',strtotime($evento->inicio_evento));
-		 	$HrIni=date('H:i', 	strtotime($evento->hora_inicio));
-		 	$fimDt=date('d-m-Y',strtotime($evento->fim_evento));
-		 	$HrFim=date('H:i', 	strtotime($evento->hora_fim));
-		?>		
-		<div style="float: left">
- 			<a href="{{route('evento.show', $evento->id)}}">
-				<div class="ui link cards">
-					<div class="green card" style="">
-						<div class="ui fluid image">
-							<label class="ui green ribbon label">{{$iniDt}} - {{$HrIni}} às {{$fimDt}} - {{$HrFim}}</label>		
-							<div class="image">
-								@isset($evento->imagem)
-									<img class="ui massive image" src="/img/evento/{{$evento->imagem}}">
-									<p>oi</p>
-								@else
-									<img class="ui massive image" src="/B.jpg">
-								@endisset
-							</div>
-						</div>
-						<div class="content">
-							<div class="header"><center>{{$evento->nome}}</center></div>
-							<div class="description" style="height: 10em;">
-								<center>
-							  <h5>{{$evento->descricao}}</h5>
-								</center>	
-							</div>
-						</div>
-						<div class="extra content">
-							<a href="{{route('evento.show', $evento->id)}}">
-								<div class="ui green inverted button" style="width: 100%;">
-									<i class="calendar icon"></i>
-									Consultar Evento
-								</div>
-							</a>
-						</div>
-					</div>
-				</div> 
-			</a>
-		</div>	
+		@foreach($eventos as  $evento)
+				
+			@component('card-evento', ['evento' => $evento, 'past' => false])
+			@endcomponent
+		
+		@endforeach
+		@foreach($past as  $evento)
+				
+			@component('card-evento', ['evento' => $evento, 'past' => true])
+			@endcomponent
+		
 		@endforeach
    @endif
 </div></center>
