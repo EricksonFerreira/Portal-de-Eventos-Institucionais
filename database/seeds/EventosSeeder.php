b8d0b2953ef212bec1f30e8366d61143aff85119
Ranieri Valença <ranierivalenca@gmail.com>
2018-11-28 11:38:39 -0300

    Melhorando seeder de eventos

--- database/seeds/EventosSeeder.php
+++ database/seeds/EventosSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class EventosSeeder extends Seeder
 {
@@ -12,8 +13,29 @@ class EventosSeeder extends Seeder
     public function run()
     {
         //
-        factory(App\Evento::class, 10)->create()->each(function ($user) {
-            // $user->posts()->save(factory(App\Post::class)->make());
+        factory(App\Evento::class, 10)->create()->each(function ($evento) {
+            factory(App\Palestrante::class, 5)->create([
+                'evento_id' => $evento->id
+            ])->each(function($palestrante) use ($evento) {
+                factory(App\Atividade::class, 2)->create([
+                    'evento_id' => $evento->id,
+                    'palestrante_id' => $palestrante->id
+                ]);
+            });
         });
+        factory(App\Evento::class, 10)
+            ->create([
+                'user_id' => User::where(['email' => 'admin@pei.ifpe'])->get()->first()->id,
+            ])
+            ->each(function ($evento) {
+                factory(App\Palestrante::class, 5)->create([
+                    'evento_id' => $evento->id
+                ])->each(function($palestrante) use ($evento) {
+                    factory(App\Atividade::class, 1)->create([
+                        'evento_id' => $evento->id,
+                        'palestrante_id' => $palestrante->id
+                    ]);
+                });
+            });
     }
 }
