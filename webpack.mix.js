64e9efda4f1b00df90fa99fd98a3047d06432950
Ranieri Valença <ranierivalenca@gmail.com>
2018-11-29 11:47:54 -0300

    Atualizando dependências nos arquivos que são construídos pelo 'npm run dev'

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .copy('node_modules/jquery/dist/jquery.js', 'public/js')
+   .copy('node_modules/semantic-ui/dist/semantic.js', 'public/js');
