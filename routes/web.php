778a712bc08b5449c2a11be5567a990a7a90d897
Erickson <erickson.rinho@hotmail.com>
2019-04-18 00:59:01 -0300

    Fazendo uma pequena organização nas rotas

--- routes/web.php
+++ routes/web.php
@@ -7,31 +7,23 @@ Route::get('/', 'EventoController@index');
 Route::get('many-to-many', 'EventoController@participar');
 
 /*Resource que facilita chamar uma função do Controller*/
-Route::resource('evento', 'EventoController');
-
-/*Resource que facilita chamar uma função do Controller*/
 Route::resource('atividade', 'AtividadeController');
 
-/*Resource que facilita chamar uma função do Controller*/
-Route::resource('palestrante', 'PalestranteController');
-
-/* Criar Atividade*/
-Route::get('/atividade/create/{id}', 'AtividadeController@create')->name('atividade.create');
 
 /* Criar Palestrante*/
 Route::get('/palestrante/create/{id}', 'PalestranteController@create')->name('palestrante.create');
 
+/*Resource que facilita chamar uma função do Controller*/
+Route::resource('palestrante', 'PalestranteController');
+
+Route::get('/palestrante/{id}/search/{query}', 'PalestranteController@search')->name('palestrante.search');
+
 /* rota tradicional do método Auth*/
 Auth::routes();
 
-/* Chamar  a função update*/
-Route::get('/evento/{id}/update', 'HomeController@update');
 
-/* Chamar  a função update*/
-Route::get('/evento/atividade/{id}/edit', 'AtividadeController@edit')->name('atividade');;
-
-/* Chamar a função upando*/
-Route::get('/evento/{id}/participar', 'HomeController@participar');
+/*Resource que facilita chamar uma função do Controller*/
+Route::resource('evento', 'EventoController');
 
 /* Chamar a função myevent*/
 Route::get('/evento/{id}/meuseventos', 'EventoController@myEvent');
@@ -42,10 +34,16 @@ Route::get('/evento/{id}/gerenciaevento', 'EventoController@gerenciaEvento')->na
 /* Chamar a função checking*/
 Route::get('/evento/{id}/checking', 'EventoController@checking')->name('evento.checking');
 
+
 /* Chamar a função destroyParticipante*/
 Route::get('/evento/{id}/destroy', 'HomeController@destroyParticipante');
+
+/* Chamar  a função update*/
+Route::get('/evento/{id}/update', 'HomeController@update');
+
+/* Chamar a função upando*/
+Route::get('/evento/{id}/participar', 'HomeController@participar');
 
 /* Vai para a função index quando for para o home*/
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/palestrante/{id}/search/{query}', 'PalestranteController@search')->name('palestrante.search');
